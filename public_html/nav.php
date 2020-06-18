03e42e23062adf22a1604aa5da8713ec1fe0cfe7
boosungkim <boosungk@protonmail.com>
2020-06-18 15:03:41 -0400

    menu with php attempt

---
+++ public_html/nav.php
@@ -0,0 +1,10 @@
+<nav class="main-nav">
+  <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href='blog.html'>Blog</a></li>
+    <li><a href='projects.html'>Projects</a></li>
+    <li><a href='resume.pdf'>Resume</a></li>
+    <li><a href='resume.pdf'>Resume</a></li>
+    <!--Maybe art-->
+  </ul>
+</nav>
