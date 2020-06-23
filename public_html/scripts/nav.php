5ebb4fd12955a1b84bb63aa1fa06a90f6c174c84
boosungkim <boosungk@protonmail.com>
2020-06-23 12:15:49 -0400

    fixing website cache issue

--- public_html/scripts/nav.php
+++ public_html/scripts/nav.php
@@ -3,8 +3,7 @@
     <li><a href="../index.php">Home</a></li>
     <li><a href='../blog.php'>Blog</a></li>
     <li><a href='../projects.php'>Projects</a></li>
-    <li><a href='../resume.pdf'>Resume</a></li>
-    <li><a href='../contactme.php'>Contact Me</a></li>
+    <li><a href='../resume2.pdf'>Resume</a></li>
     <li><a href='../contactme.php'>Contact Me</a></li>
     <!--Maybe art-->
   </ul>
