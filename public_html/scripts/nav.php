d181d35ed9266f3030a3fa41c889b08e3bcbc454
boosungkim <boosungk@protonmail.com>
2020-07-26 20:30:41 -0400

    updated resume

--- public_html/scripts/nav.php
+++ public_html/scripts/nav.php
@@ -5,7 +5,7 @@
     <li><a href="/index.php">Home</a></li>
     <li><a href='/blog.php'>Blog</a></li>
     <li><a href='/projects.php'>Projects</a></li>
-    <li><a href='/resume.pdf'>Resume</a></li>
+    <li><a href='/resume2020.pdf'>Resume</a></li>
     <li><a href='/contactme.php'>Contact Me</a></li>
   </ul>
 </nav>
